331f7e790f45629a597b37c079cfaba07d38eba5
Sergey Madaminov <smadaminov@cs.stonybrook.edu>
2020-04-20 05:54:04 +0000

    added icmp struct; modified fxn signature to pass target address; pretty much completed the send part of the icmp echo-reply

--- include/ping_handler.hpp
+++ include/ping_handler.hpp
@@ -1,6 +1,15 @@
 #ifndef __PING_HANDLER_HPP
 #define __PING_HANDLER_HPP
 
-void send_another_ping(const int& ping_sock_fd);
+#include <netinet/ip_icmp.h>
+
+#define ICMP_PKT_SIZE 64
+
+typedef struct icmp_pkt {
+    struct icmphdr header;
+    char msg[ICMP_PKT_SIZE - sizeof(struct icmphdr)];
+} icmp_pkt;
+
+int send_another_ping(const int& ping_sock_fd, const struct sockaddr_in& target_address);
 
 #endif /* __PING_HANDLER_HPP */
--- src/ping_handler.cc
+++ src/ping_handler.cc
@@ -1,6 +1,38 @@
 #include <iostream>
-#include "ping_handler.hpp"
+#include <array>
+#include <numeric>
+#include <algorithm>
 
-void send_another_ping(const int& ping_sock_fd) {
-    std::cout << "Ping not implemented yet..." << std::endl;
+#include <sys/types.h>
+#include <unistd.h>
+#include <strings.h>
+
+#include "ping_handler.hpp"
+#include "ping_cli.hpp"
+#include "checksum.hpp"
+
+// Let's not create a copy of a descriptor but rather pass it by reference
+int send_another_ping(const int& ping_sock_fd, const struct sockaddr_in& target_address) {
+    PingCliProperties *ping_cli = PingCliProperties::get_properties();
+
+    icmp_pkt pkt;
+    std::array<int, sizeof(pkt.msg) - 1> pkt_size;
+    std::iota(pkt_size.begin(), pkt_size.end(), 1);
+
+    bzero(&pkt, sizeof(pkt));
+    pkt.header.type = ICMP_ECHO;
+    pkt.header.un.echo.id = getpid();
+
+    // Let's do C++ Lambda expression becease `why not?`
+    std::for_each(pkt_size.begin(), pkt_size.end(), [&pkt](int i){ pkt.msg[i - 1] = (i - 1) + '0'; });
+    pkt.msg[pkt_size.size()] = 0;
+    pkt.header.un.echo.sequence = ping_cli->get_message_count();
+    pkt.header.checksum = checksum(&pkt, sizeof(pkt));
+
+    ping_cli->inc_message_count();
+
+    if (sendto(ping_sock_fd, &pkt, sizeof(pkt), 0, (struct sockaddr*)&target_address, sizeof(target_address)) <= 0) {
+        std::cerr << "Cannot send ICMP message." << std::endl;
+        return -1;
+    }
 }
